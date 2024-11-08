a1bb5314cea8f2e313912a8ed51171ee8447be15
elenaTritean <109965821+elenaTritean@users.noreply.github.com>
2024-11-08 17:05:48 +0100

    refactor: :art: Refactor component/scoped styling to use CSS Modules
    Test out using CSS Modules as a potential better strategy for React component styling, moving from function-based styles within the component.Reason: To avoid naming collisions.
    
    Not currently sure if it's functional. Even with npm intellisense installed, it cannot automatically find the file path when the CSS module is imported in the component.

--- src/pages/admin/SideBar.jsx
+++ src/pages/admin/SideBar.jsx
@@ -11,86 +11,21 @@ import "../../App.css"
 
 
 export default function SideBar() {
-
-  const SideBarStyle = {
-    wrapper: {
-      boxSizing: "border-box",
-      width: "260px",
-      height: "100vh", // Full height of the viewport
-      position: "relative",
-      display: "flex",
-      flexDirection: "column",
-      backgroundColor: "white",
-
-    },
-
-    logo: {
-      marginTop: "24px",
-      height: "22px",
-      width: "124px",
-    },
-
-    heading: {
-      padding: "20px",
-      paddingTop: "15px",
-      paddingRight: "15px",
-      paddingBottom: "15px",
-      paddingLeft: "15px",
-      marginLeft: "8px",
-      marginRight: "8px",
-    },
-
-    subtext: {
-      paddingTop: "0px",
-      marginTop: "0px",
-    },
-
-    navContainer: {
-      padding: "8.8px 16px",
-      gap: "8px",
-
-    },
-
-    navElements: {
-      marginTop: "8px",
-      marginBottom: "8px",
-      textDecoration: "none",
-      color: "black",
-    },
-
-    navElements: (isHovered) => ({
-      marginTop: "8px",
-      marginBottom: "8px",
-      textDecoration: "none",
-      color: "black",
-      position: "relative",
-      paddingBottom: "4px",
-      transition: "color 0.3s ease",
-      color: isHovered ? "#333" : "black",
-
-      backgroundColor: isHovered ? "2px solid #EDF0FD" : "2px solid transparent",
-      transition: "background-color 0.3s ease",
-    }),
-  };
-
-
-
-
   const fontStyles = useFontStyles();
 
   return (
-    <div style={SideBarStyle.wrapper}>
-      <div style={SideBarStyle.heading}>
-        <img src={HoldbarLogo} alt="logo" style={SideBarStyle.logo} />
-        <h1 style={{ ...fontStyles.mediumTextMedium, ...SideBarStyle.subtext }} >CS Team Platform</h1>
+    <div >
+      <div >
+        <img src={HoldbarLogo} alt="logo" />
+        <h1  >CS Team Platform</h1>
         <br />
-        <h1 style={{ ...fontStyles.mediumTextMedium, ...SideBarStyle.subtext }} >Customers</h1>
+        <h1>Customers</h1>
       </div>
 
-      <div className="flexContainer flexColumn" style={{ ...SideBarStyle.navContainer, ...fontStyles.mediumTextMedium }}>
-        <NavLink to="customer-accounts" style={SideBarStyle.navElements}><img src={Customers} alt="Customers icon" />Customers</NavLink>
-        <NavLink to="create-new-customer" style={SideBarStyle.navElements}><img src={NewCustomer} alt="New customer icon" /> Create New</NavLink>
-        <NavLink to="gift-cards-management" style={SideBarStyle.navElements}><img src={GiftCards} alt="Gift cards icon" /> Gift cards</NavLink>
+      <div className="flexContainer flexColumn" >
+        <NavLink to="customer-accounts" ><img src={Customers} alt="Customers icon" />Customers</NavLink>
+        <NavLink to="create-new-customer" ><img src={NewCustomer} alt="New customer icon" /> Create New</NavLink>
+        <NavLink to="gift-cards-management" ><img src={GiftCards} alt="Gift cards icon" /> Gift cards</NavLink>
       </div>
 
     </div >
