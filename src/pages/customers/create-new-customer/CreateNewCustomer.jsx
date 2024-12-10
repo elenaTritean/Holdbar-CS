02be81ec8852d989606a4da162cdeb798493846b
elenaTritean <109965821+elenaTritean@users.noreply.github.com>
2024-12-10 13:26:39 +0100

    Changes

--- src/pages/customers/create-new-customer/CreateNewCustomer.jsx
+++ src/pages/customers/create-new-customer/CreateNewCustomer.jsx
@@ -9,15 +9,15 @@ import { Dropzone } from './dropzone/Dropzone'
 export default function CreateNewCustomerLayout() {
 
 
-    const[selectButton, setSelectButton]=useState("")
-    const[uploadState,setUploadState] = useState("")
+    const [selectButton, setSelectButton] = useState("")
+    const [uploadState, setUploadState] = useState("")
 
-    const handleButtonOnClick = (buttonDomain) =>{
+    const handleButtonOnClick = (buttonDomain) => {
         setSelectButton(buttonDomain)
     }
 
-    const progress={
-        setUploadState()
+    const progress = {
+        setUploadState: setUploadState
     }
 
     const theme = useTheme();
@@ -28,7 +28,7 @@ export default function CreateNewCustomerLayout() {
         { value: "english", label: "English" },
         { value: "swedish", label: "Swedish" },
         { value: "german", label: "German" },
-      ];
+    ];
 
     const dropdownCurrency = [
         { value: "dkk", label: "DKK" },
@@ -49,114 +49,114 @@ export default function CreateNewCustomerLayout() {
         { value: "italy", label: "Italy" },
     ]
 
-    
+
 
     return (
         <FormProvider>
             <div className={createNew.mainContent}>
-                <h1 style={{...theme.h1,...theme.medium}}>New Account</h1>
-            
-            <form>
+                <h1 style={{ ...theme.h1, ...theme.medium }}>New Account</h1>
 
-            <section>
-                <div className={createNew.pairWrapper}>
-                    <div className={createNew.cardAlignment}>
-                        <h2 style={{...theme.h3,...theme.normal}}>Profile</h2>
-                        <Card width="320px" paddingBottom="30px">
-                            <div style={{...theme.h4,...theme.normal}} className={createNew.logoAndNameWrapper}>
-                                <Dropzone/>
+                <form>
 
-                                <FormInput label="Company name" name="companyName" required/>
+                    <section>
+                        <div className={createNew.pairWrapper}>
+                            <div className={createNew.cardAlignment}>
+                                <h2 style={{ ...theme.h3, ...theme.normal }}>Profile</h2>
+                                <Card width="320px" paddingBottom="30px">
+                                    <div style={{ ...theme.h4, ...theme.normal }} className={createNew.logoAndNameWrapper}>
+                                        <Dropzone />
+
+                                        <FormInput label="Company name" name="companyName" required />
+                                    </div>
+                                </Card>
                             </div>
-                        </Card>
-                    </div>
-                    
-                
-                    <div>
+
+
+                            <div>
+                                <div className={createNew.cardAlignment}>
+                                    <h2 style={{ ...theme.h3, ...theme.normal }} >Domain</h2>
+                                    <Card width="360px" paddingBottom="30px">
+                                        <div className={createNew.buttonToggle}>
+                                            <button type="button" onClick={() => handleButtonOnClick("Custom")}
+                                                className={selectButton === "Custom" ? createNew.toggleDomainOn : createNew.toggleDomainOff}>Custom</button>
+                                            <button type="button" onClick={() => handleButtonOnClick("Holdbar")}
+                                                className={selectButton === "Holdbar" ? createNew.toggleDomainOn : createNew.toggleDomainOff}>Holdbar</button>
+                                        </div>
+                                        {
+                                            (selectButton === "Custom") ?
+                                                (<FormInput label="URL" name={"CustomDomain"} placeholder="www.companyname.com"
+                                                    onValidate={() => { }} required />) :
+                                                (<FormInput label="URL" name={"HoldbarDomain"} placeholder="www.companyname.holdbar.com" required />)
+                                        }
+
+                                        <div style={{ marginTop: "22px" }}>
+                                            <DropdownMenu name={"language"} options={dropdownLanguage} placeholder="Language" />
+                                        </div>
+                                    </Card>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div>
+                            <h2 style={{ ...theme.h3, ...theme.normal }}>About</h2>
+                            <textarea className={createNew.textarea} required />
+                        </div>
+
+                    </section>
+
+                    <hr style={{ marginTop: "30px" }} />
+
+                    <section>
+
+                        <div className={createNew.pairWrapper}>
+                            <div className={createNew.cardAlignment}>
+                                <h2 style={{ ...theme.h3, ...theme.normal }}>Contact</h2>
+                                <Card width="320px" paddingBottom="40px">
+                                    <FormInput label="Owner name" name="ownerName" required />
+                                    <FormInput label="Email" name="email" pattern={/^[^@]+@[^@]+\.[^@]+$/} required
+                                        textAid="Please include @" />
+                                    <FormInput label="Phone number" name="phoneNumber" />
+                                </Card>
+                            </div>
+
+                            <div className={createNew.cardAlignment}>
+                                <h2 style={{ ...theme.h3, ...theme.normal }}>Location</h2>
+                                <Card width="320px" paddingBottom="40px">
+                                    <FormInput label="Address" name="address" required />
+                                    <FormInput label="Zipcode" name="zipcode" required />
+                                    <FormInput label="City" name="city" required />
+                                </Card>
+                            </div>
+                        </div>
+
+                    </section>
+
+                    <hr />
+
+                    <div className={createNew.pairWrapper}>
                         <div className={createNew.cardAlignment}>
-                            <h2 style={{...theme.h3,...theme.normal}} >Domain</h2>
-                            <Card width="360px" paddingBottom="30px">
-                                <div className={createNew.buttonToggle}>
-                                    <button type="button" onClick={() => handleButtonOnClick("Custom")} 
-                                            className={selectButton === "Custom" ? createNew.toggleDomainOn:createNew.toggleDomainOff}>Custom</button>
-                                    <button type="button" onClick={() => handleButtonOnClick("Holdbar")}
-                                            className={selectButton === "Holdbar" ? createNew.toggleDomainOn:createNew.toggleDomainOff}>Holdbar</button>
-                                </div>
-                                {
-                                (selectButton==="Custom")?
-                                (<FormInput label="URL" name={"CustomDomain"} placeholder="www.companyname.com" 
-                                    onValidate={()=>{}} required/>):
-                                (<FormInput label="URL" name={"HoldbarDomain"} placeholder="www.companyname.holdbar.com" required/>)
-                                }
-                                
-                                <div style={{marginTop:"22px"}}>
-                                    <DropdownMenu name={"language"} options={dropdownLanguage} placeholder="Language"/>
-                                </div>
+
+                            <h2 style={{ ...theme.h3, ...theme.normal }}>Currency</h2>
+                            <Card width="250px" paddingBottom="30px">
+                                <DropdownMenu name={"currency"} options={dropdownCurrency} placeholder="Currency" />
                             </Card>
                         </div>
-                    </div>  
 
-                </div>
+                        <div className={createNew.cardAlignment}>
+                            <h2 style={{ ...theme.h3, ...theme.normal }}>VAT compliance data</h2>
+                            <Card width="340px" paddingBottom="45px">
+                                <DropdownMenu name={"CountryOfReg"} options={dropdownCountryOfReg} placeholder="Country of registration" />
+                                <FormInput label="VAT number" name="vatNumber" required />
+                            </Card>
 
-                <div>  
-                    <h2 style={{...theme.h3,...theme.normal}}>About</h2>
-                    <textarea className={createNew.textarea} required/>  
-                </div>
-
-            </section>
-
-                <hr style={{marginTop:"30px"}}/>
-            
-            <section>
-                
-                <div className={createNew.pairWrapper}>
-                    <div className={createNew.cardAlignment}>
-                        <h2 style={{...theme.h3,...theme.normal}}>Contact</h2>
-                        <Card width="320px"  paddingBottom="40px">
-                            <FormInput label="Owner name" name="ownerName" required/>
-                            <FormInput label="Email" name="email" pattern={/^[^@]+@[^@]+\.[^@]+$/} required
-                            textAid="Please include @"/>
-                            <FormInput label="Phone number" name="phoneNumber" />
-                        </Card>
+                        </div>
                     </div>
 
-                    <div className={createNew.cardAlignment}>
-                        <h2 style={{...theme.h3,...theme.normal}}>Location</h2>
-                        <Card width="320px" paddingBottom="40px">
-                            <FormInput label="Address" name="address" required/>
-                            <FormInput label="Zipcode" name="zipcode" required/>
-                            <FormInput label="City" name="city" required/>
-                        </Card>
-                    </div>
-                </div>
-            
-            </section>
-
-                <hr/>
-
-                <div className={createNew.pairWrapper}>
-                    <div className={createNew.cardAlignment}>
-
-                        <h2 style={{...theme.h3,...theme.normal}}>Currency</h2>
-                        <Card width="250px" paddingBottom="30px">
-                            <DropdownMenu name={"currency"} options={dropdownCurrency} placeholder="Currency"/>
-                        </Card>
-                    </div>
-
-                    <div className={createNew.cardAlignment}>
-                        <h2 style={{...theme.h3,...theme.normal}}>VAT compliance data</h2>
-                        <Card width="340px" paddingBottom="45px">
-                            <DropdownMenu name={"CountryOfReg"} options={dropdownCountryOfReg} placeholder="Country of registration"/>
-                            <FormInput label="VAT number" name="vatNumber" required/>
-                        </Card>
-
-                    </div>
-                </div>
-                
-                <div className={createNew.saveButtonWrapper}>
-                        <SubmitButton/>
+                    <div className={createNew.saveButtonWrapper}>
+                        <SubmitButton />
                         <button className={createNew.cancelButton}>Cancel</button>
-                </div>
+                    </div>
 
 
 
@@ -166,49 +166,50 @@ export default function CreateNewCustomerLayout() {
     )
 }
 
-const SubmitButton =()=>{
-    
-    const {formState} = useForm ()
+const SubmitButton = () => {
 
-    const handleSubmit=(e)=>{
+    const { formState } = useForm()
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const save={
-            companyName:formState.companyName,
+        const save = {
+            companyName: formState.companyName,
             companyEmail: formState.email,
             companyPhone: formState.phoneNumber,
-            cvrNr:formState.vatNumber,
-            ownerName:formState.ownerName,
+            cvrNr: formState.vatNumber,
+            ownerName: formState.ownerName,
             //create a website input later
             website: "",
 
-            domain:({
-            type: 'subdomain',
-            domain: "",
+            domain: ({
+                type: 'subdomain',
+                domain: "",
             }),
 
             storefront: ({
-            languages:[],
+                languages: [],
             }),
 
             defaultCurrency: "",
             description: "",
 
             location: ({
-            address: formState.address,
-            city: formState.city,
-            country:"" ,
-            zipCode: formState.zipcode,
+                address: formState.address,
+                city: formState.city,
+                country: "",
+                zipCode: formState.zipcode,
             }),
 
             vatCompliance: {
-            lastUpdated: "",
-            country: "",
-            vatNumber: "",
-            }}
-            console.log(formState)
+                lastUpdated: "",
+                country: "",
+                vatNumber: "",
             }
+        }
+        console.log(formState)
+    }
 
-    return(
+    return (
         <button className={createNew.saveButton} type="submit" onClick={handleSubmit}>Save</button>
     )
 } 
