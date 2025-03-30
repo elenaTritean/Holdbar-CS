81fe3baa740daa4217bf6c8d7c3bf13d978b5ee3
elenaTritean <elena@tritean.com>
2025-03-30 20:46:47 +0200

    Update Table.jsx

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,55 +1,9 @@
-import React from 'react'
-import {useTable} from "react-table"
-import table from "./Table.module.css"
-import { useTheme } from "../styling/ThemeContext"
+import React, { useMemo } from 'react'
 
-export default function Table({data, columns}) {
-  
-  
-  const theme = useTheme();
-  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable ({columns, data});
-  const dateFormatter = new Intl.DateTimeFormat("en-US", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
+export default function Table() {
 
-  
+ 
   return (
-    <div>
-      <table {...getTableProps()} className={table.tableWrapper} >
-        <thead className={table.theadWrapper} style={theme.h5}>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps)} style={{...theme.h5,...theme.normal}} onClick={column.initialSortOrder}>
-                  {column.render("Header")}
-                  <button
-                    
-                  />
-                </th>
-                ))} 
-            </tr>
-
-           )
-           )}
-        </thead>
-
-        <tbody {...getTableBodyProps()} className={table.tbodyWrapper} style={{ ...theme.h4, ...theme.greyColor }}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()} >
-                {row.cells.map((cell) => {
-                  return (
-                    <td data-label={cell.column.id} {...cell.getCellProps()}>
-                      {cell.render("Cell")}
-                    </td>)})}
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+    <div>Table</div>
   )
 }
